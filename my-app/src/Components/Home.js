412969885701590e3cd492e6c1fcdac6b57f1e17
balkaran singh <balkarnsingh6134@gmail.com>
2026-05-19 10:09:15 +0530

    add

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -206,7 +206,7 @@ const Home = () => {
         <div className="container4">
           <img src="https://m.media-amazon.com/images/I/61I7ejSq1GL._AC_SL1500_.jpg" alt="Image 1" />
           <div className="overlay-text">The Phone For Seamless Wireless Charging</div>
-         <Link to="/shop" className="see-more-link">See More</Link>
+          <a> <Link to="/shop" className="see-more-link">See More</Link></a>
         </div>
 
         {/* Overlay Card 2 */}
